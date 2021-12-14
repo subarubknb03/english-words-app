1830e4de684eb3ce6c04df3891303335b2cdec9f
subarubknb03 <subaru519kqka@gmail.com>
2021-12-14 15:58:05 +0900

    Revert "update start"
    This reverts commit d174899c3711981efe0b6b15b84a1adbfe0d46ff.

--- windows/start.bat
+++ windows/start.bat
@@ -3,7 +3,7 @@ if not exist .env (
     python -m venv .env
     .env\Scripts\activate
     pip3 install --upgrade pip setuptools
-    pip3 install -r .\app\windows\requirements.txt
+    pip3 install -r .\windows\requirements.txt
     cd app
     python main.py
 ) else (
